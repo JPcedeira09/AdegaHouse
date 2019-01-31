0bf3c7e65f255459f312cbee78cf6aa83f210eb5
kaueBAuricchio <46527280+kaueBAuricchio@users.noreply.github.com>
2019-01-31 09:25:24 -0200

    foi feito a tela de cadastro login e a tela de visualizaçao do dono

--- app/src/main/java/com/example/guiay/adegahouse/fragment/addProdutosFragment.java
+++ app/src/main/java/com/example/guiay/adegahouse/fragment/addProdutosFragment.java
@@ -8,11 +8,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.guiay.adegahouse.R;
+import com.example.guiay.adegahouse.activity.AdicionarProduto;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class addProdutosFragment extends Fragment {
+
+    private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference("Produtos" );
 
 
 
@@ -24,6 +29,20 @@ public class addProdutosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        DatabaseReference Produtos = referencia.child("Teste");
+
+        AdicionarProduto adicionarProduto = new AdicionarProduto();
+        adicionarProduto.setDescriçao("Teste");
+        adicionarProduto.setNome("kaue");
+        adicionarProduto.setDisponivel(false);
+        adicionarProduto.setQuantidade(10);
+        adicionarProduto.setValor(13);
+
+        Produtos.child("teste").setValue(Produtos);
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_produto, container, false);
     }
